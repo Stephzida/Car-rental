3bb69790f99fc179f653ad74782ec4e64d9896d1
Stephen <stephenanosike18@gmail.com>
2025-06-18 15:00:40 +0100

    first commit

--- src/components/Naviagtion.jsx
+++ src/components/Naviagtion.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
+import { useState } from 'react';
 import Button from './Button'
 import {Container} from 'react-bootstrap';
 
 const Naviagtion = () => {
+      const [showLinks,setShowlinks] = useState(false)
+
+      const handleActive = () =>{
+       setShowlinks(!showLinks)
+      }
   return (
     <div className="hero-cover">
     <Container className='nav'>
-        <button className='burger'><i className="bi bi-list"></i></button>
-        
+        <div className='burgerBtn'>
+          <button className={`burger`} onClick={handleActive}><i className="bi bi-list"></i></button>
+        <div className="headerNavBlock">
+          {showLinks ?   <div className="headerNavlink">
+            <a href="">Rent</a>
+                    <a href="">Share</a>
+                    <a href="">About us</a>
+                    <a href="">Contact</a>
+          </div> : "" }
+           
+        </div>
+        </div>
+
+        <span className='logo'>LUXEDRIVE</span>
         <Button message={"Login/Register"} active={"true"}/>
+        
     </Container>
     </div>
   )
